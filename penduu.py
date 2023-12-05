b620abcfa8012c9cd788b0788330294f99314579
AdAbd <adezzed@gmail.com>
2023-12-05 09:16:12 +0100

    Pendu partie2

--- penduu.py
+++ penduu.py
@@ -12,3 +12,18 @@ def afficher_mot_cache(mot, lettres_trouvees):
         else:
             mot_cache += "_"
     return mot_cache
+    def deviner_lettre():
+    while True:
+        lettre = input("Devinez une lettre : ").lower()
+        if len(lettre) == 1 and lettre.isalpha():
+            return lettre
+        else:
+            print("Veuillez entrer une seule lettre valide.")
+
+def jouer_pendu():
+    mot_a_deviner = choisir_mot()
+    lettres_trouvees = []
+    essais_restants = 9
+
+    print("Bienvenue au jeu du pendu !")
+    print("Le mot à deviner a", len(mot_a_deviner), "lettres."
